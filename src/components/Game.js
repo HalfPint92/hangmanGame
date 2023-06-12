75ff6c4d4759f12ebe8a74479f02e7c1cf39d013
Aaron Carter <aaron.dk01@gmail.com>
2023-06-12 23:14:02 +0200

    added styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,79 +5,81 @@ import HangmanImage from './HangmanImage';
 import './Game.css';
 
 const Game = () => {
-  const [targetWord, setTargetWord] = useState('');
-  const [guessedLetters, setGuessedLetters] = useState([]);
-  const [remainingGuesses, setRemainingGuesses] = useState(10);
+    const [targetWord, setTargetWord] = useState('');
+    const [guessedLetters, setGuessedLetters] = useState([]);
+    const [remainingGuesses, setRemainingGuesses] = useState(10);
 
-  const fetchWordFromDictionary = async () => {
-    try {
-      const response = await fetch('/dictionary.txt');
-      const text = await response.text();
-      const words = text.split('\n').filter((word) => word.length > 0);
-      const randomIndex = Math.floor(Math.random() * words.length);
-      const randomWord = words[randomIndex].toLowerCase();
-      setTargetWord(randomWord);
-    } catch (error) {
-      console.error('Error fetching word from dictionary:', error);
-    }
-  };
+    const fetchWordFromDictionary = async () => {
+        try {
+            const response = await fetch('/dictionary.txt');
+            const text = await response.text();
+            const words = text.split('\n').filter((word) => word.length > 0);
+            const randomIndex = Math.floor(Math.random() * words.length);
+            const randomWord = words[randomIndex].toLowerCase();
+            setTargetWord(randomWord);
+        } catch (error) {
+            console.error('Error fetching word from dictionary:', error);
+        }
+    };
 
-  useEffect(() => {
-    fetchWordFromDictionary();
-  }, []);
+    useEffect(() => {
+        fetchWordFromDictionary();
+    }, []);
 
-  const handleLetterClick = (letter) => {
-    if (!guessedLetters.includes(letter)) {
-      setGuessedLetters([...guessedLetters, letter]);
+    const handleLetterClick = (letter) => {
+        if (letter !== '-' && !guessedLetters.includes(letter)) {
+            setGuessedLetters([...guessedLetters, letter]);
 
-      if (!targetWord.includes(letter)) {
-        setRemainingGuesses(remainingGuesses - 1);
-      }
-    }
-  };
+            if (!targetWord.includes(letter)) {
+                setRemainingGuesses(remainingGuesses - 1);
+            }
+        }
+    };
 
-  const handleRestartGame = () => {
-    fetchWordFromDictionary(); // Fetch a new word
-    setGuessedLetters([]);
-    setRemainingGuesses(10);
-  };
+    const handleRestartGame = () => {
+        fetchWordFromDictionary(); 
+        setGuessedLetters([]);
+        setRemainingGuesses(10);
+    };
 
-  const gameWon = targetWord
-    .split('')
-    .every((letter) => guessedLetters.includes(letter));
+    const gameWon = targetWord
+        .split('')
+        .every((letter) => guessedLetters.includes(letter));
 
-  const gameLost = remainingGuesses === 0;
+    const gameLost = remainingGuesses === 0;
 
-  return (
-    <div className="game-container">
-      <h1>Hangman Game</h1>
-      <WordDisplay targetWord={targetWord} guessedLetters={guessedLetters} />
-      <HangmanImage remainingGuesses={remainingGuesses} />
-      <div className="game-message">
-        {gameWon && <p className="win-message">You won the game!</p>}
-        {gameLost && <p className="lose-message">You lost the game!</p>}
-      </div>
-      <div>
-        {gameWon || gameLost ? (
-          <button onClick={handleRestartGame}>Restart Game</button>
-        ) : (
-          <div>
-            <p>Remaining Guesses: {remainingGuesses}</p>
-            <div>
-              {Array.from('abcdefghijklmnopqrstuvwxyz').map((letter) => (
-                <LetterButton
-                  key={letter}
-                  letter={letter}
-                  onClick={handleLetterClick}
-                  disabled={guessedLetters.includes(letter)}
-                />
-              ))}
+    return (
+        <div className="game-container">
+            <h1>Hangman Game</h1>
+            <WordDisplay targetWord={targetWord} guessedLetters={guessedLetters} />
+            <HangmanImage remainingGuesses={remainingGuesses} />
+            <div className="game-message">
+                {gameWon && <p className="win-message">You won the game!</p>}
+                {gameLost && <p className="lose-message">
+                    You lost the game!<br></br>The correct word was: {targetWord}
+                </p>}
             </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+            <div>
+                {gameWon || gameLost ? (
+                    <button className="button-restart" onClick={handleRestartGame}>Restart Game</button>
+                ) : (
+                    <div>
+                        <p className="remaining-guesses">Remaining Guesses: {remainingGuesses}</p>
+                        <div>
+                            {Array.from('abcdefghijklmnopqrstuvwxyz').map((letter) => (
+                                <LetterButton
+                                    key={letter}
+                                    letter={letter}
+                                    onClick={handleLetterClick}
+                                    disabled={guessedLetters.includes(letter)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Game;
